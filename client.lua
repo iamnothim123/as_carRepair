704b115eacc4ef047405a78e5ce2663ebc3a7815
iamnothim123 <128757345+iamnothim123@users.noreply.github.com>
2024-12-12 07:26:04 -0800

    add notification

--- client.lua
+++ client.lua
@@ -6,5 +6,9 @@ local vehicle = GetVehiclePedIsIn(ped, false)
 if IsPedInAnyVehicle(ped, false) then
     SetVehicleFixed(vehicle)
     SetVehicleDirtLevel(vehicle, 0.0)
+
+    BeginTextCommandDisplayHelp("STRING")
+    AddTextComponentSubstringPlayerName("Your vehicle has been repaired!")
+    EndTextCommandDisplayHelp(0, false, true, 5000)
 end,
 false)
