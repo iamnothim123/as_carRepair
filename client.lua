c3c78ddac1046ee810b1b35c2ac8143d3839b36e
iamnothim123 <128757345+iamnothim123@users.noreply.github.com>
2024-12-11 20:28:04 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,11 @@
-RegisterCommand("repairveh", function(source, args, rawCommand), 
-    
+RegisterCommand("repairveh", 
+function(source, args, rawCommand)
+local ped = PlayerPedId()
+local vehicle = GetVehiclePedIsIn(ped, false)
+
+if IsPedInAnyVehicle(ped, false) then
+    SetVehicleEngineHealth(vehicle, 1000)
+    SetVehicleBodyHealth(vehicle, 1000)
+
+end,
 false)
