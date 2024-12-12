720816d2c79d09aadd17e3f5eb1fb28ea3e17a06
iamnothim123 <128757345+iamnothim123@users.noreply.github.com>
2024-12-12 07:15:57 -0800

    change native for fixing vehicle and clean car

--- client.lua
+++ client.lua
@@ -4,8 +4,7 @@ local ped = PlayerPedId()
 local vehicle = GetVehiclePedIsIn(ped, false)
 
 if IsPedInAnyVehicle(ped, false) then
-    SetVehicleEngineHealth(vehicle, 1000)
-    SetVehicleBodyHealth(vehicle, 1000)
-
+    SetVehicleFixed(vehicle)
+    SetVehicleDirtLevel(vehicle, 0.0)
 end,
 false)
